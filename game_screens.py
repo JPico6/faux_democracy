a465b1d5137e27bea0228466ebf2bdcdf690149e
jp <jason.piccone@gmail.com>
2022-10-12 21:44:40 -0400

    build in-game menus

--- game_screens.py
+++ game_screens.py
@@ -3,10 +3,12 @@ import pygame_menu
 from create_world import initiate_game
 from pygame.locals import *
 import sys
+import json
 
 color_white = (255, 255, 255)
 color_black = (0, 0, 0)
 surface = pygame.display.set_mode((1000, 600))
+game_initiated = False
 
 
 def main_menu():
@@ -45,7 +47,7 @@ def game_start(name_box, state_name_box):
 
     def close_menu():
         menu.disable()
-        main_game_screen(player_name, state_name)
+        main_game_screen(player_name, state_name, True)
 
     player_name = name_box.get_value()
     state_name = state_name_box.get_value()
@@ -55,25 +57,54 @@ def game_start(name_box, state_name_box):
     menu.mainloop(surface)
 
 
-def game_screen():
+def main_game_screen(player_name, state_name, initiate=False):
 
-    while True:
+    def pop_menu():
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+        def close_menu():
+            menu.disable()
+            main_game_screen(None, None)
 
+        menu = pygame_menu.Menu('Modern Democracy: The Game!', 600, 400,
+                                theme=pygame_menu.themes.THEME_BLUE)
+        menu.add.button('Close Menu', close_menu)
+        menu.mainloop(surface)
 
-def main_game_screen(player_name, state_name):
+    def approval_menu():
+        def close_menu():
+            menu.disable()
+            main_game_screen(player_name, state_name)
 
-#    clock = pygame.time.Clock()
+        menu = pygame_menu.Menu('Modern Democracy: The Game!', 600, 400, theme=pygame_menu.themes.THEME_BLUE)
+        menu.mainloop(surface)
+        menu.add.button('Close', close_menu)
+
+    def currency_menu():
+        def close_menu():
+            menu.disable()
+            main_game_screen(player_name, state_name)
+
+        menu = pygame_menu.Menu('Modern Democracy: The Game!', 600, 400, theme=pygame_menu.themes.THEME_BLUE)
+        menu.mainloop(surface)
+        menu.add.button('Close', close_menu)
+
+    def in_game_menu(inpt):
+        if inpt == 1:
+            pop_menu()
+        if inpt == 2:
+            approval_menu()
+        if inpt == 3:
+            currency_menu()
 
     # create player-state attributes
-    game_data = initiate_game()
-    game_data['game_dat']['player_name'] = player_name
-    game_data['game_dat']['state_name'] = state_name
-    print(game_data)
+    if initiate:
+        game_data = initiate_game()
+        game_data['game_dat']['player_name'] = player_name
+        game_data['game_dat']['state_name'] = state_name
+        print(game_data)
+    if not initiate:
+        with open('data/turn_dat.json') as f:
+            game_data = json.load(f)
 
     pygame.font.init()
     pygame.display.set_caption('Modern Democracy: The Game!')
@@ -114,19 +145,27 @@ def main_game_screen(player_name, state_name):
         # setup a font for displaying inventory numbers
         INVFONT = pygame.font.Font('freesansbold.ttf', 12)
 
-        # display select game attributes
-        place_position = 10
-        for item in display_attributes:
-            DISPLAYSURF.blit(textures[item], (place_position, MAPHEIGHT * TILESIZE + 10))
-            place_position += 30
-            textObj = INVFONT.render(str(display_attributes[item]), True, color_white, color_black)
-            DISPLAYSURF.blit(textObj, (place_position, MAPHEIGHT * TILESIZE + 10))
-            place_position += 50
-
         # so you can click out of - quit the game
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # display select game attributes
+            place_position = 10
+            for item in display_attributes:
+                DISPLAYSURF.blit(textures[item], (place_position, MAPHEIGHT * TILESIZE + 10))
+                place_position += 30
+                textObj = INVFONT.render(str(display_attributes[item]), True, color_white, color_black)
+                DISPLAYSURF.blit(textObj, (place_position, MAPHEIGHT * TILESIZE + 10))
+                place_position += 50
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+
+                    if DISPLAYSURF.blit(textures[item], (place_position-70, MAPHEIGHT * TILESIZE + 10)).collidepoint(pos):
+                        in_game_menu(item)
+
+
 
 
